88f58e8eff1d3ce8d051bf725e1799f7f01d5541
Ryan Galimova <ryan7falcon@gmail.com>
2022-05-14 22:33:11 -0400

    init

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+export default {
+  moduleNameMapper: {
+    '#(.*)': '<rootDir>/node_modules/$1',
+  },
+};
---
+++ src/game.test.js
@@ -0,0 +1,7 @@
+import { jest } from '@jest/globals'
+
+describe('game', () => {
+  it('game should have state and board', () => {
+    expect(1).toBe(1)
+  })
+})
