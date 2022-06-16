7434f674d30f1da2145e11cc68729b43f1a78481
Ryan Galimova <ryan7falcon@gmail.com>
2022-06-16 17:45:28 -0400

    boards

---
+++ src/boards.js
@@ -0,0 +1,7 @@
+const board1 = [
+  [true, true, true],
+  [true, true, false],
+  [false, false, true],
+]
+
+export { board1 }
--- src/start.js
+++ src/start.js
@@ -2,13 +2,8 @@ import {
   print,
 } from './util.js'
 import { step, repeatToConvergenceOrMax } from './game.js'
-
-const board = [
-  [true, true, true],
-  [true, true, false],
-  [false, false, true],
-]
+import { board1 } from './boards.js'
 
 print('Game of life')
 
-repeatToConvergenceOrMax(10)(step)(board)
+repeatToConvergenceOrMax(10)(step)(board1)
